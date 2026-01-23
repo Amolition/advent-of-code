74fe8a91c01490fd0178354d1777ce0f6008c412
Amol Bhuptani <amolbhuptani@gmail.com>
2026-01-23 20:33:44 +0000

    refactored 2025 day 2 part 1 solution to be more efficient and hopefully make it easier to generalise for part 2

--- rust/2025/aoc02/src/main.rs
+++ rust/2025/aoc02/src/main.rs
@@ -8,15 +8,14 @@ fn main() {
         panic!("Input must be ASCII only")
     };
     // separate ranges into distinct strings
-    let bc_ranges = raw_input.split(",");
+    let bc_ranges = raw_input.trim().split(",");
     let invalid_id_sum = bc_ranges.fold(0, |acc, el| {
         let (min_str, max_str) = el.split_once("-").expect("Invalid range format");
-        let min: u64 = min_str.trim().parse().expect("Invalid number");
-        let max: u64 = max_str.trim().parse().expect("Invalid number");
-        println!("range: {}-{}", min, max);
+        let min: u64 = min_str.parse().expect("Invalid number");
+        let max: u64 = max_str.parse().expect("Invalid number");
 
         let min_len = min.ilog10() + 1;
-        let mut seq_no = if min_len.is_multiple_of(2) {
+        let first = if min_len.is_multiple_of(2) {
             let (half1, half2) = min_str.split_at((min_len / 2).try_into().unwrap());
             let half1: u64 = half1.parse().expect("Invalid number");
             let half2: u64 = half2.parse().expect("Invalid number");
@@ -24,14 +23,21 @@ fn main() {
         } else {
             10_u64.pow(min_len / 2)
         };
-        let mut val = seq_no + seq_no * 10_u64.pow(seq_no.ilog10() + 1);
-        println!("{}", seq_no);
+
+        let max_len = max.ilog10() + 1;
+        let last = if max_len.is_multiple_of(2) {
+            let (half1, half2) = max_str.split_at((max_len / 2).try_into().unwrap());
+            let half1: u64 = half1.parse().expect("Invalid number");
+            let half2: u64 = half2.parse().expect("Invalid number");
+            if half1 <= half2 { half1 } else { half1 - 1 }
+        } else {
+            10_u64.pow(max_len / 2) - 1
+        };
+
         let mut sum = acc;
-        while val <= max {
-            println!("{val}");
+        for seq in first..(last + 1) {
+            let val = seq + seq * 10_u64.pow(seq.ilog10() + 1);
             sum += val;
-            seq_no += 1;
-            val = seq_no + seq_no * 10_u64.pow(seq_no.ilog10() + 1);
         }
         sum
     });
